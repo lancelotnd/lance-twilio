a691a53062f99a66bdeecff79fb59829be06185c
lancelotnd <32873563+lancelotnd@users.noreply.github.com>
2017-10-27 15:57:15 -0400

    enqueue

--- assignment.php
+++ assignment.php
@@ -1,8 +1,10 @@
 <?php
 
 $assignment_instruction = [
-  'instruction' => 'accept'
+  'instruction' => 'dequeue',
+  'post_work_activity_sid' => '{WA9bb03b33359a99bbd29b8d04f275cc45}',
+  'from' => '+14505002017' // a verified phone number from your twilio account
 ];
 
 header('Content-Type: application/json');
-echo json_encode($assignment_instruction);  
+echo json_encode($assignment_instruction);
