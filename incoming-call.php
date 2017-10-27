1abd9bf56ffbc0c14fd6ada4e88192c1bd1e600f
lancelotnd <32873563+lancelotnd@users.noreply.github.com>
2017-10-27 14:52:24 -0400

    incoming-call v2

--- incoming-call.php
+++ incoming-call.php
@@ -4,7 +4,7 @@ header("Content-Type: application/xml; charset=utf-8");
 
 <Response>
   <Gather action="enqueue-call.php" numDigits="1" timeout="5">
-    <Say language="fr">Merci d'avoir appeller Lancelot!/Say>
+    <Say language="fr">Merci d'avoir appeller Lancelot!</Say>
     <Say language="fr">Pour le français, faites le 1.</Say>
     <Say language="en">For English, please hold or press two.</Say>
   </Gather>
