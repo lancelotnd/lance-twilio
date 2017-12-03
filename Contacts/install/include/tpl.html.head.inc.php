ce5759373578cbfbdaafb7e367a0894e10b89c4c
lancelotnd <32873563+lancelotnd@users.noreply.github.com>
2017-12-03 13:45:37 -0500

    Folder install Deleted

--- Contacts/install/include/tpl.html.head.inc.php
+++
@@ -1,4 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php $css = _LANG_FILE_( "css/style.##LANG_CODE##.css" ); ?>
-<link href="<?php echo $css; ?>" rel="stylesheet" type="text/css"/>
-<title>Lancelot's Contacts&nbsp;<?php echo RSTR_INSTALL_INSTALLATION; ?></title>
